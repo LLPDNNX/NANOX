13535de87a014ecba976fe42c7a2237acd3ae564
matt-komm <Matthias.Komm@cern.ch>
2018-05-29 18:30:33 +0100

    moved docker setup into script

---
+++ test/runCMSSWTest.sh
@@ -0,0 +1,19 @@
+function run_test()
+{
+    touch /var/lib/rpm/* || return 1
+    yum -y install yum-plugin-ovl || return 1
+    yum -y install glibc-devel.x86_64 --disablerepo=adobe\* || return 1
+    cd ~
+    source ~/.bashrc
+    export SCRAM_ARCH=slc6_amd64_gcc630 || return 1
+    scramv1 project CMSSW CMSSW_9_4_8 || return 1
+    cd CMSSW_9_4_8/src || return 1
+    eval \`scramv1 runtime -sh\` || return 1
+    git config --global user.email "root.rooster@chicken.com" || return 1
+    git config --global user.name "Root rooster"
+    git cms-merge-topic cms-nanoAOD:master || return 1
+    git clone https://github.com/LLPDNNX/NANOX.git || return 1
+    scram b || return 1
+}
+
+run_test
