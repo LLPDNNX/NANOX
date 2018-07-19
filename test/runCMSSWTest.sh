33ae1e744e6a4a1509d5f532c6e82a123f9863b3
matt-komm <Matthias.Komm@cern.ch>
2018-07-19 19:11:54 +0100

    update cmssw version to 949

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -9,8 +9,8 @@ function run_test()
     cd ~
     source ~/.bashrc
     export SCRAM_ARCH=slc6_amd64_gcc630 || return 1
-    scramv1 project CMSSW CMSSW_9_4_8 || return 1
-    cd CMSSW_9_4_8/src || return 1
+    scramv1 project CMSSW CMSSW_9_4_9 || return 1
+    cd CMSSW_9_4_9/src || return 1
     eval `scramv1 runtime -sh` || return 1
     git config --global user.email "root.rooster@chicken.com"
     git config --global user.name "Root rooster"
