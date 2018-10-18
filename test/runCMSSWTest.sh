7aa9d489ba0036da663e672d21a43ed9ab8f9739
matt-komm <Matthias.Komm@cern.ch>
2018-10-18 16:15:30 +0100

    fix nanoaod version

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -1,3 +1,19 @@
+function cms-merge-commit()
+{
+    local github_user="${1}"
+    local topic_branch="${2}"
+    local commit_hash="${3}"
+    echo "-----> Merging commit ${commit_hash} on branch ${topic_branch} from ${github_user}"
+
+    git remote add -t "${topic_branch}" "${github_user}" "https://github.com/${github_user}/cmssw.git"
+    git fetch --no-tags "${github_user}" "${topic_branch}:refs/remotes/${github_user}/${topic_branch}" || { echo "Could not fetch branch ${topic_branch} from ${github_user}"; exit 1; }
+    local current_branch="$(git rev-parse --abbrev-ref HEAD)"
+    local merge_base="$(git merge-base ${commit_hash} ${current_branch})"
+    git cms-sparse-checkout "${merge_base}" "${commit_hash}"
+    git read-tree -mu HEAD
+    git merge --no-ff -m "Merged ${commit_hash} on branch ${topic_branch} from ${github_user} into ${current_branch}" "${commit_hash}" || { echo "Could not merge ${commit_hash} on branch ${topic_branch} from ${github_user} into ${current_branch}"; exit 1; }
+}
+
 function run_test()
 {
     #check siteconfig for GT
@@ -14,7 +30,8 @@ function run_test()
     eval `scramv1 runtime -sh` || return 1
     git config --global user.email "root.rooster@chicken.com"
     git config --global user.name "Root rooster"
-    git cms-merge-topic cms-nanoAOD:master || return 1
+    git init
+    cms-merge-commit cms-nanoAOD master a348544b0d03cbf135953ce23c27ea2b4723327c
     mkdir NANOX
     rsync -r --stats /scripts/ NANOX/. || return 1
     scram b || return 1
